f7b2fe5b451b82fcd8f58ec3775eb65cd094ce84
Patrick M <tatater56@gmail.com>
2020-05-28 13:09:37 -0400

    blocked out home view layout

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -7,13 +7,40 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   final Color _mainColor = Colors.red[900];
+  final double _padding = 20;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: _mainColor,
       child: SafeArea(
-        child: Center(child: Text('home')),
+        child: Padding(
+          padding: EdgeInsets.all(_padding),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildLastWorkoutCard(),
+              buildNextWorkoutCard(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildLastWorkoutCard() {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.only(bottom: _padding),
+      child: SizedBox(height: 100, child: Text("Last workout")),
+    );
+  }
+
+  Widget buildNextWorkoutCard() {
+    return Expanded(
+      child: Container(
+        color: Colors.white,
+        child: Text("Next workout"),
       ),
     );
   }
