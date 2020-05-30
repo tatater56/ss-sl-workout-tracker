003b355d9e7bc633af0241a27a96fe7ee6db94f7
Patrick M <tatater56@gmail.com>
2020-05-29 23:06:24 -0400

    separate "main_screen" from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:bottom_navy_bar/bottom_navy_bar.dart';
-
-import 'views/main_pages/home.dart';
-import 'views/main_pages/history.dart';
-import 'views/main_pages/settings.dart';
+import 'package:ss_sl_workout_tracker/views/main_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,72 +9,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final String _appbarTitle = 'SS / SL Workout Tracker';
-  int _pageIndex = 0;
-  PageController _pageController;
-
-  @override
-  void initState() {
-    super.initState();
-    _pageController = PageController();
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         textTheme: TextTheme(bodyText2: TextStyle(fontSize: 20.0)),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(_appbarTitle,
-              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-        ),
-        extendBodyBehindAppBar: true,
-        extendBody: true,
-        body: Center(
-          child: PageView(
-            controller: _pageController,
-            onPageChanged: (index) {
-              setState(() => _pageIndex = index);
-            },
-            children: <Widget>[
-              HomeView(),
-              HistoryView(),
-              SettingsView(),
-            ],
-          ),
-        ),
-        bottomNavigationBar: BottomNavyBar(
-          selectedIndex: _pageIndex,
-          onItemSelected: (index) {
-            setState(() => _pageIndex = index);
-            _pageController.animateToPage(index,
-                duration: Duration(milliseconds: 270), curve: Curves.ease);
-          },
-          backgroundColor: Colors.transparent,
-          showElevation: false,
-          curve: Curves.ease,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          items: [
-            buildBottomNavyBarItem(Icons.home, 'HOME'),
-            buildBottomNavyBarItem(Icons.history, 'HISTORY'),
-            buildBottomNavyBarItem(Icons.settings, 'SETTINGS'),
-          ],
-        ),
-      ),
-    );
-  }
-
-  BottomNavyBarItem buildBottomNavyBarItem(IconData iconData, String title) {
-    return BottomNavyBarItem(
-      icon: Icon(iconData),
-      title: Text(title),
-      activeColor: Colors.white,
-      inactiveColor: Colors.white,
-      textAlign: TextAlign.center,
+      debugShowCheckedModeBanner: false,
+      title:  "SS/SL Workout Tracker",
+      home: MainScreen(),
+      routes: {
+        // "/splash": (_) => Splash(),
+        // "/workout": (_) => Workout(),
+        "/main_screen": (_) => MainScreen(),
+      },
     );
   }
 }
---
+++ lib/views/main_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+
+import 'main_pages/home.dart';
+import 'main_pages/history.dart';
+import 'main_pages/settings.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final String _appbarTitle = 'SS / SL Workout Tracker';
+  int _pageIndex = 0;
+  PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_appbarTitle,
+            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      extendBodyBehindAppBar: true,
+      extendBody: true,
+      body: Center(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() => _pageIndex = index);
+          },
+          children: <Widget>[
+            HomeView(),
+            HistoryView(),
+            SettingsView(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _pageIndex,
+        onItemSelected: (index) {
+          setState(() => _pageIndex = index);
+          _pageController.animateToPage(index,
+              duration: Duration(milliseconds: 270), curve: Curves.ease);
+        },
+        backgroundColor: Colors.transparent,
+        showElevation: false,
+        curve: Curves.ease,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        items: [
+          buildBottomNavyBarItem(Icons.home, 'HOME'),
+          buildBottomNavyBarItem(Icons.history, 'HISTORY'),
+          buildBottomNavyBarItem(Icons.settings, 'SETTINGS'),
+        ],
+      ),
+    );
+  }
+
+  BottomNavyBarItem buildBottomNavyBarItem(IconData iconData, String title) {
+    return BottomNavyBarItem(
+      icon: Icon(iconData),
+      title: Text(title),
+      activeColor: Colors.white,
+      inactiveColor: Colors.white,
+      textAlign: TextAlign.center,
+    );
+  }
+}
