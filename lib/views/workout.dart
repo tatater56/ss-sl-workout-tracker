545bd5f69b85f65ac5f9e1ce9f1e63a157c1bdb7
Patrick M <tatater56@gmail.com>
2020-06-02 02:48:10 -0400

    workout view block out exercises

--- lib/views/workout.dart
+++ lib/views/workout.dart
@@ -41,15 +41,57 @@ class _WorkoutViewState extends State<WorkoutView> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  ExerciseWidget(),
-                  SizedBox(height: _margins),
-                  ExerciseWidget(),
+                  // ExerciseWidget(),
+                  // SizedBox(height: _margins),
+                  // ExerciseWidget(),
+                  // SizedBox(height: _margins),
+                  _buildExerciseFocused('Squat', 1, 135, 140),
+                  //SizedBox(height: _margins),
+                  _buildExerciseUnfocused('OHP', 0, 95),
+                  _buildExerciseUnfocused('DL', 0, 185)
                 ],
               )),
         ),
       ),
     );
   }
+}
+
+Widget _buildExerciseFocused(
+    String name, int progress, int oldWeight, int targetWeight) {
+  return Container(
+    decoration: _decoration,
+    padding: const EdgeInsets.all(_padding),
+    child: Stack(
+      children: <Widget>[
+        Align(alignment: Alignment.centerLeft, child: Text(name)),
+        Align(alignment: Alignment.center, child: Text('Set $progress / 5')),
+        Align(
+            alignment: Alignment.centerRight,
+            child: Text(
+                '$oldWeight + ${targetWeight - oldWeight}\n$targetWeight lb')),
+      ],
+    ),
+  );
+}
+
+Widget _buildExerciseUnfocused(String name, int progress, int targetWeight) {
+  return Padding(
+    padding: const EdgeInsets.all(_padding).copyWith(bottom: _padding / 2),
+    child: DefaultTextStyle(
+      style: TextStyle(color: Colors.white, fontSize: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(child: Text('$name', textAlign: TextAlign.left)),
+          Expanded(
+              child: Text(progress == 0 ? '5 x 5' : '$progress / 5',
+                  textAlign: TextAlign.center)),
+          Expanded(child: Text('$targetWeight lb', textAlign: TextAlign.right)),
+        ],
+      ),
+    ),
+  );
 }
 
 class ExerciseWidget extends StatefulWidget {
